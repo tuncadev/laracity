5978b0846b660ac2234631ba62702bf00cb6012a
tuncadev <tunca.development@gmail.com>
2023-12-29 16:20:38 +0200

    hero bg

--- resources/views/components/job-list-hero.blade.php
+++ resources/views/components/job-list-hero.blade.php
@@ -1,10 +1,9 @@
 <form  method="POST" action="">
-<div {{$attributes->merge(['class' => 'project-hero  w-full bg-blue-500 align-center'])}}>
+<div {{$attributes->merge(['class' => 'project-hero  w-fullalign-center'])}}>
 	<div class="max-w-lg md:max-w-7xl md:m-auto container  items-center text-center text-white pt-20 pb-20">
 		<h1 class="text-white text-2xl font-bold mb-4">{{__('head.hero.h1')}}</h1>
 		<div class="search-projects flex justify-center w-sm md:w-full align-center relative">
 			<div class="searchbox" >
-
 				<i class="fa-solid fa-magnifying-glass text-gray-500 absolute top-4 text-xl pl-3"></i>
 				<input type="text" name="search" class="bg-gray-50 rounded-tl-lg rounded-bl-lg text-gray-600 px-6 py-4 pl-10 text-md max-w-min md:w-96">
 			</div>
